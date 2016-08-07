30f7a956b8a14eb69e5f32fa8d4df3760e525834
Petr Gazarov <petrgazarov@gmail.com>
2016-08-07 00:25:30 -0400

    Optimize error handling and page parsing

---
+++ link_crawler/link_crawler.rb
@@ -0,0 +1,95 @@
+require 'csv'
+require_relative './website_parser.rb'
+require 'httparty'
+require 'pry'
+
+class LinkCrawler
+  NUM_ATTEMPTS = 2
+  RESULTS_FILE = 'link_crawler/results.csv'
+
+  def initialize(source_file)
+    @source_file = source_file
+  end
+
+  def visit_websites
+    websites = File.readlines(source_file).uniq.map(&:chomp)
+
+    websites.each do |url_address|
+      begin
+        visit_website(url_address)
+      rescue Exception => e
+        debug("#{url_address}: unhandled exception occured")
+
+        next
+      end
+    end
+  end
+
+  private
+
+  def visit_website(url_address)
+    attempt = 1
+
+    begin
+      debug("#{url_address}: visiting #{attempt} time") if attempt == NUM_ATTEMPTS
+
+      response = HTTParty.get(url_address, { timeout: 45 })
+
+      record_entry(url_address, response)
+
+    rescue URI::InvalidURIError
+      record_error(url_address, 'Invalid URL')
+
+    rescue OpenSSL::SSL::SSLError
+      record_error(url_address, 'URL Error: try changing https to http')
+
+    rescue Net::OpenTimeout
+      record_error(url_address, 'Unable to load website')
+
+    rescue Errno::ECONNREFUSED
+      record_error(url_address, 'Connection error: is URL valid?')
+
+    rescue Zlib::BufError, HTTParty::RedirectionTooDeep
+      record_error(url_address, 'Error loading website')
+
+    rescue Exception => e
+      if attempt == NUM_ATTEMPTS
+        record_unhandled_error(url_address, e)
+      else
+        attempt += 1
+
+        retry
+      end
+    end
+  end
+
+  def record_entry(url_address, response)
+    parser = WebsiteParser.parse(html: response, url_address: url_address)
+
+    CSV.open(RESULTS_FILE, "a+") do |csv|
+      csv << [url_address, parser.status, parser.keywords]
+    end
+  end
+
+  def record_error(url_address, status)
+    CSV.open(RESULTS_FILE, "a+") do |csv|
+      csv << [url_address, status]
+    end
+  end
+
+  def record_unhandled_error(url_address, error)
+    CSV.open(RESULTS_FILE, "a+") do |csv|
+      csv << [url_address, 'UNHANDLED ERROR', nil, error, error.backtrace.inspect]
+    end
+  end
+
+  def debug(message)
+    puts message
+  end
+
+  attr_reader :source_file
+end
+
+puts "started at #{Time.now}"
+LinkCrawler.new("#{Dir.pwd}/link_crawler/links.txt").visit_websites
+puts "finished at #{Time.now}"
---
+++ link_crawler/website_parser.rb
@@ -0,0 +1,67 @@
+require 'nokogiri'
+require 'active_support'
+require 'active_support/core_ext'
+
+class WebsiteParser
+  KEYWORDS = [
+    'parenting',
+    'children',
+    'kids',
+    'finance',
+    'money'
+  ]
+
+  attr_reader :status, :keywords
+
+  def self.parse(html:, url_address:)
+    new(html: html, url_address: url_address)
+  end
+
+  def initialize(html:, url_address:)
+    @html = html
+    @url_address = url_address
+
+    parse_html
+  end
+
+  private
+
+  def parse_html
+    parse_status
+    parse_keywords
+  end
+
+  def parse_status
+    @status = (html.code == 200 ? 'SUCCESS' : "FAIL (#{html.code})")
+  end
+
+  def parse_keywords
+    keywords_regex = KEYWORDS.join('|')
+
+    if status == 'SUCCESS'
+      @keywords = (relevant_html.scan(/(#{keywords_regex})/) + url_address.scan(/(#{keywords_regex})/))
+        .uniq
+        .join(', ')
+    end
+  end
+
+  def relevant_html
+    parsed_page = Nokogiri::HTML(html)
+
+    body_and_title = parsed_page.at('body').try(:text).to_s + parsed_page.at('title').try(:text).to_s
+    body_and_title = body_and_title.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+
+    remove_script_and_style_tags!(body_and_title)
+
+    body_and_title.downcase
+  end
+
+  def remove_script_and_style_tags!(html_body)
+    script_tags = html_body.scan(/\<script.*\<\/script\>/)
+    style_tags = html_body.scan(/\<style.*\<\/style\>/)
+
+    (script_tags + style_tags).each { |tag| html_body.slice!(tag) }
+  end
+
+  attr_reader :html, :url_address
+end
