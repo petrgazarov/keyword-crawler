2d2f7ced11e0b47b8ac03b48c5766c3e56c5a391
Petr Gazarov <petr@policygenius.com>
2016-10-12 10:40:21 -0400

    Create run.rb

--- link_crawler/link_crawler.rb
+++ link_crawler/link_crawler.rb
@@ -104,7 +104,3 @@ class LinkCrawler
 
   attr_reader :source_file
 end
-
-puts "started at #{Time.now}"
-LinkCrawler.new("#{Dir.pwd}/link_crawler/links.txt").visit_websites
-puts "finished at #{Time.now}"
